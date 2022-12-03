0e2463c45cf5028012ba158bf0288d2c4e9ebab9
Dennis <dennisjr89@gmail.com>
2022-12-03 13:32:31 -0500

    day2 part two

--- 2022/day-02/index.js
+++ 2022/day-02/index.js
@@ -26,3 +26,24 @@ const gamePossibilities = {
 input.forEach((game) => {
     console.log(myScore += gamePossibilities[game])
 })
+
+// Part two!
+
+// X means need to lose.
+// Y means end round in draw
+// Z means to win.
+const gameOutcome = {
+    'A X': 3,
+    'B X': 1,
+    'C X': 2,
+    'A Y': 4,
+    'B Y': 5, 
+    'C Y': 6,
+    'A Z': 8,
+    'B Z': 9,
+    'C Z': 7, 
+}
+
+input.forEach((game) => {
+    console.log(myScore += gameOutcome[game])
+})
