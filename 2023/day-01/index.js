7ddffac24de5a2cc9e852661bcb9a79e9febc7c2
Dennis Campos <dennisjr89@gmail.com>
2023-12-03 17:42:56 -0500

    yuck

--- 2023/day-01/index.js
+++ 2023/day-01/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
-const calibration = fs.readFileSync('./index.txt', 'utf-8').split('\n');
+const calibration = fs.readFileSync('./index.txt', 'utf-8').split('\n')
 
-
+// Part one
 function getTotalDigits(calibration) {
     let total = 0; 
 
@@ -32,3 +32,32 @@ function getTotalDigits(calibration) {
 }
 
 console.log('Answer: ', getTotalDigits(calibration))
+
+// part two
+function convertNumber(string) {
+    const digits = string
+        .replace(/one/g, 'one1one')
+        .replace(/two/g, 'two2two')
+        .replace(/three/g, 'three3three')
+        .replace(/four/g, 'four4four')
+        .replace(/five/g, 'five5five')
+        .replace(/six/g, 'six6six')
+        .replace(/seven/g, 'seven7seven')
+        .replace(/eight/g, 'eight8eight')
+        .replace(/nine/g, 'nine9nine')
+        .replace(/\D/g, '')
+
+    return Number(digits[0] + digits.slice(-1))
+}
+
+function partTwo(nums) {
+    let sum = 0
+
+    nums.forEach((num) => {
+        sum += convertNumber(num)
+    })
+
+    return sum;
+}
+
+console.log('Answer: ', partTwo(calibration))
